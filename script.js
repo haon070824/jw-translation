79cfb2894c90db8d0f6fd34af9faa366a3b18a3d
haon070824 <sohome33@naver.com>
2025-01-17 14:19:27 +0900

    프로젝트 업데이트: HTTPS 환경 준비 및 코드 개선

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 // 신권 용어 사전
 const customTerms = {
+    "エホバ": "여호와",
+    "神": "하느님",
     "王国会館": "왕국회관",
     "奉仕": "봉사",
     "長老": "장로",
-    "会衆": "회중"
+    "会衆": "회중",
+    "イエス": "예수",
+    "キリスト": "그리스도",
+    "主権": "주권",
+    "統治": "통치",
+    "大祭司": "대제사장",
+    "千年統治": "천년 통치",
+    "パラダイス": "낙원",
+    "兄弟": "형제",
+    "姉妹": "자매",
+    "祈り": "기도",
+    "野外奉仕": "야외 봉사",
+    "生活と奉仕の集会": "그리스도인 생활과 봉사 집회",
+    "公開講演": "공개 강연",
+    "割り当て": "과제",
+    "聖句": "성구",
+    "聖書": "성경",
+    "祈りの終わり": "기도의 끝",
+    "閉会の祈り": "마치는 기도",
+    "開会の祈り": "시작하는 기도",
+    "兄弟姉妹": "형제 자매",
+    "地上": "땅",
+    "神の王国": "하느님의 왕국",
+    "神の目的": "하느님의 목적",
+    "生活と奉仕": "생활과 봉사",
+    "公開証言": "공개 증언",
+    "証拠": "증거",
+    "会議": "회의",
+    "王国": "왕국",
+    "主": "주",
+    "学び": "공부",
+    "弟子": "제자",
+    "信仰": "신앙",
+    "希望": "희망",
+    "愛": "사랑"
 };
 
 let isListening = false;
@@ -72,7 +108,7 @@ async function translateText(japaneseText) {
 
         // 신권 용어 변환
         for (const [term, translation] of Object.entries(customTerms)) {
-            translatedText = translatedText.replace(term, translation);
+            translatedText = translatedText.replace(new RegExp(term, 'g'), translation);
         }
 
         document.getElementById("translated-text").value = translatedText;
